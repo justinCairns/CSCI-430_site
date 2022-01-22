1e7e50560cf9e17838cf8a666b502f9c20e15c3a
Justin Cairns <Jcairns@eagles.bridgewater.edu>
2022-01-22 12:28:16 -0500

    updated app.js again.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+const dir = path.join(__dirname, "../public")
+app.use(express.static(dir))
+
+app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+})
+
+const port = process.env.PORT || 3000 
+
+app.listen(port, () => { 
+    console.log('Server is up on port ' + port) 
+})
