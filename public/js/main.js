73b9734e0b034e772b84105733c9513caf6dfa74
Justin Cairns <Jcairns@eagles.bridgewater.edu>
2022-03-14 09:45:02 -0400

    added ability to update account name and password properties using my API.

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
 const protocol = window.location.protocol
 const host = window.location.host
 
+
+
 //account info
 const displayAccountItem = document.querySelector("#displayAccountItem")
 
@@ -31,6 +33,47 @@ displayAccountItem.addEventListener("click", async(e) => {
     } else {
         console.log("HTTP-Error: " + response.status)
     }
+})
+
+//modify account
+const modifyAccountModalSaveButton = document.querySelector("#modifyAccountModalSaveButton")
+modifyAccountModalSaveButton.addEventListener("click", async(e) => {
+    e.preventDefault()
+
+    const token = localStorage.getItem("token")
+
+    //const url = "http://localhost:3001/users/me"
+    const url = 'https://justincairns-rest-api.herokuapp.com/users/me'
+
+    const nameInput = document.querySelector("#nameInput")
+    const passwordInput = document.querySelector("#passwordInput")
+    const name = nameInput.value
+    const password = passwordInput.value
+    const requestData = {...name && { name }, ...password && { password } }
+    //console.log(requestData)
+
+    const options = {
+        method: "PATCH",
+        headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestData),
+    }
+
+    let response = await fetch(url, options)
+
+    if (response.status === 200) {
+        const contentArea = document.querySelector("#contentArea")
+        contentArea.innerHTML = `Saved successful.`
+    } else {
+        console.log("HTTP-Error: " + response.status)
+    }
+
+    const modal = document.querySelector("#modifyAccountModal")
+    bootstrap.Modal.getInstance(modal).hide()
+
+    const form = document.querySelector("#modifyAccountForm").reset()
 })
 
 //delete account
